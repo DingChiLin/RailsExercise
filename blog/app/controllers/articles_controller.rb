6b2fffe8d8288ad7db6aff82738c061431181445
DingChiLin <gn01168178@yahoo.com.tw>
2015-04-05 20:04:02 +0800

    add show.html.erb and finish show added new article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,11 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
+
   end
 
   def create
@@ -8,4 +14,5 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to @article
   end
+
 end
