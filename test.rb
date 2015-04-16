ac2e4109e508d48cdbc3901475d20f2ea6330062
DingChiLin <gn01168178@yahoo.com.tw>
2015-04-16 08:48:45 +0800

    change test.rb

--- test.rb
+++ test.rb
@@ -1,1 +1,19 @@
-puts "test HA"
+class Test
+
+  def initialize(ttt)
+    @ttt = ttt
+  end
+
+  def print
+    puts @ttt
+  end
+
+  def print2
+    puts ttt
+  end
+
+end
+
+test = Test.new("HI Test")
+test.print
+test.print2
