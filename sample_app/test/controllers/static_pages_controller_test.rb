fcd6e7bfc231b1452c368eb8d806ff19f7311e54
DingChiLin <gn01168178@yahoo.com.tw>
2015-05-11 18:35:16 +0800

    add user model

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -9,19 +9,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "#{@title_body}"
+    assert_select "title", full_title
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@title_body}"
+    assert_select "title", full_title("Help")
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@title_body}"
+    assert_select "title", full_title("About")
   end
 
   test "should get contact" do
